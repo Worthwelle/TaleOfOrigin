4a68705b424b2fa3212f8bc32a842d9e64f9562b
Kiran Welle <me@kiranwelle.com>
2017-10-06 15:00:51 -0700

    Set the name and version of the app

--- config/app.php
+++ config/app.php
@@ -12,7 +12,19 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'Laravel',
+    'name' => 'Tale of Origin',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Application Version
+    |--------------------------------------------------------------------------
+    |
+    | This value is the version of your application. This value is used when the
+    | framework needs to place the application's version in a notification or
+    | any other location as required by the application or its packages.
+    */
+
+    'version' => '0.0',
 
     /*
     |--------------------------------------------------------------------------
